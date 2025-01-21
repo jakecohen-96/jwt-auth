c014f4b900d5105e57fb51219d9645005200f645
jakecohen-96 <yakop.96@gmail.com>
2025-01-21 20:22:13 +0200

    comitting

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const knex = require("knex");
 require("dotenv").config();
 
+const { PGCONNECTIONURI } = process.env;
+
 module.exports = {
   db: knex({
     client: "pg",
-    connection: process.env.PGCONNECTIONURI,
-    ssl: { rejectUnauthorized: false }, // Ensure SSL is properly configured for Render
+    connection: PGCONNECTIONURI,
+    ssl: { rejectUnauthorized: false }, // Render requires SSL
   }),
 };
