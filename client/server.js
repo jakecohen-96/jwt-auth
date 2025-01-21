38b3ef207b39b16484da546d78c9c5b0b7ecb88c
jakecohen-96 <yakop.96@gmail.com>
2025-01-21 19:37:50 +0200

    dependany changes plus server changes

--- client/server.js
+++ client/server.js
@@ -1,18 +1,18 @@
-
 import express from "express";
-import cors from cors;
+import cors from "cors";
 import path from "path";
 
 const app = express();
-app.use(cors());
 
-app.listen(process.env.PORT || 3001, () => {
-    console.log(`running on port ${process.env.PORT || 3001}`);
-});
+app.use(cors());
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/dist")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "/dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`running on port ${process.env.PORT || 3001}`);
 });
--- server.js
+++ server.js
@@ -2,28 +2,28 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const userRouter = require("./routes/userRouter.js");
+const path = require("path");
 
 const app = express();
 
-const path = require("path");
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://jwt-auth-x16v.onrender.com"],
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", userRouter);
+
 app.use(express.static(path.join(__dirname, "client", "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
 const { PORT } = process.env;
-
 app.listen(PORT || 5001, () => {
-  console.log(`run on ${PORT || 5001}`);
+  console.log(`Server running on port ${PORT || 5001}`);
 });
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    // origin: ["http://localhost:5173"],
-  })
-);
-
-app.use("/api/user", userRouter);
