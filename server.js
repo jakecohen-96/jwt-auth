11f3fd8d9c1b85127acbbdc19243c091c0aecfa7
jakecohen-96 <yakop.96@gmail.com>
2025-01-21 18:45:53 +0200

    update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const userRouter = require("./routes/userRouter.js");
 
 const app = express();
 
-// const path = require("path");
-// app.use(express.static(path.join(__dirname, "client", "dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-// });
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
 const { PORT } = process.env;
 
